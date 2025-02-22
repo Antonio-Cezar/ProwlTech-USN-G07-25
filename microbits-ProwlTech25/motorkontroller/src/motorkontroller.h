5db81c36321462c3669db0951dc26e4689b5d4f7
Antonio-Cezar <cezaravadanei@gmail.com>
2025-02-22 10:40:15 +0100

    update code files

--- microbits-ProwlTech25/motorkontroller/src/motorkontroller.h
+++
@@ -1,8 +0,0 @@
-#ifndef MOTORCONTROLLER_H
-#define MOTORCONTROLLER_H
-#include <stdbool.h>
-// Function prototypes
-void control_motors(int angle, int speed, bool joystick_nr);
-int mapValue(int value, int direction);
-int calculate_speed(int duty_cycle);
-#endif
