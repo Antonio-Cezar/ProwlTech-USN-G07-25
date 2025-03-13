edbfdce553d33bb60ea617a0c9e4c4b13c250ac7
RameezSadiq <112103778+RameezSadiq@users.noreply.github.com>
2025-03-13 13:01:15 +0100

    Endret kode og la til linjer
    Rettet på feil i kode, la til kontrollstatus ved oppstart, la til riktige indekser, la til for løkke for å  gå igjennom aksene, ga aksene navn, fjernet print som printet konstant. nye variabler

--- GUI-touch-screan-ProwlTech25/raspberry_pi_can_script/xbox_one_kontroller_com.py
+++ GUI-touch-screan-ProwlTech25/raspberry_pi_can_script/xbox_one_kontroller_com.py
@@ -19,53 +19,96 @@ if pygame.joystick.get_count() == 0:
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
+#Printer ut informasjonen på kontrollen når den starter opp
 print(f"Kontroller: {joystick.get_name()} tilkoblet.")
+print("Antall knapper på kontrollen som er aktive:", joystick.get_numbuttons())
+print("Antall akser på kontrollen som er aktive:", joystick.get_numaxes())
+
+#lagrer forrige akseverdi for å unngå printing når verdi ikke endres
+num_axes = joystick.get_numaxes() #henter alle aksene kontrollen har
+siste_akse = [0.0] * joystick.get_numaxes() #lager en liste for å lagre den forrige verdien til aksen
+terksel = 0.05 #lager en terskelverdi for hvor mye aksen må endre seg før den kan skrives ut.
 
 try:
     while True:
         pygame.event.pump()
         
-        # Lesing av A-knappen (button 1 på Xbox-kontroller (A))
+        # Lesing av A-knappen (button 0 på Xbox-kontroller (A))
+        if joystick.get_button(0):
+            print("A-knappen er trykket (Indeks 0)") #indeksnummer er pygame sitt referansenummer for knappen
+            GPIO.output(25, GPIO.HIGH)
+        else:
+            GPIO.output(25, GPIO.LOW)
+
+        # Lesing av B-knappen (button 1 på Xbox-kontroller (B))
         if joystick.get_button(1):
-            print("A-knappen er trykket")
+            print("B-knappen er trykket (indeks 1)") #indeksnummer er pygame sitt referansenummer for knappen
             GPIO.output(25, GPIO.HIGH)
         else:
             GPIO.output(25, GPIO.LOW)
 
-        # Lesing av B-knappen (button 2 på Xbox-kontroller (B))
+        # Lesing av X-knappen (button 2 på Xbox-kontroller (X))
         if joystick.get_button(2):
-            print("B-knappen er trykket")
+            print("X-knappen er trykket (indeks 2)")
             GPIO.output(25, GPIO.HIGH)
         else:
             GPIO.output(25, GPIO.LOW)
 
-        # Lesing av X-knappen (button 3 på Xbox-kontroller (X))
+        # Lesing av Y-knappen (button 3 på Xbox-kontroller (Y))
         if joystick.get_button(3):
-            print("X-knappen er trykket")
+            print("Y-knappen er trykket (indeks 3)")
             GPIO.output(25, GPIO.HIGH)
         else:
             GPIO.output(25, GPIO.LOW)
 
-        # Lesing av Y-knappen (button 4 på Xbox-kontroller (Y))
-        if joystick.get_button(4):
-            print("Y-knappen er trykket")
+        # Lesing av LB-knappen rotasjon (button 9 på Xbox-kontroller (LB))
+        if joystick.get_button(9):
+            print("LB-knappen er trykket (indeks 9)")
             GPIO.output(25, GPIO.HIGH)
         else:
             GPIO.output(25, GPIO.LOW)
 
-        # Lesing av LB-knappen rotasjon (button 5 på Xbox-kontroller (LB))
-        if joystick.get_button(5):
-            print("LB-knappen er trykket")
+        # Lesing av RB-knappen rotasjon (button 10 på Xbox-kontroller (RB))
+        if joystick.get_button(10):
+            print("RB-knappen er trykket (indeks 10)")
             GPIO.output(25, GPIO.HIGH)
         else:
             GPIO.output(25, GPIO.LOW)
 
-        # Lesing av RB-knappen rotasjon (button 6 på Xbox-kontroller (RB))
-        if joystick.get_button(6):
-            print("RB-knappen er trykket")
-            GPIO.output(25, GPIO.HIGH)
-        else:
-            GPIO.output(25, GPIO.LOW)
+        #BRUKT FOR DEBUGGING AV KNAPPEINDEKS
+        #print("Sjekker hvilken knapp som tilhører hvilken verdi på kontrollen", flush=True)
+        #for i in range(joystick.get_numbuttons()): #for å iterere over alle knapper til kontrollen
+            #try:
+                #if joystick.get_button(i):
+                    #print(f"Button {i} er trykket", flush=True)  #sjekker om en knapp er trykket og printer ut knappen
+            #except Exception as e:
+                #print(f"Feil ved lesing av button {i}: {e}", flush=True) #flush brukes for å printe ut med engang.
+
+         #leser joystick-aksene
+          for i in range(num_axes): #itererer over alle aksene, variabel i er indeks til hver akse
+            try:
+                akse_verdi = joystick.get_axis(i) #henter nåværende akseverdi (mellom 1 og -1)
+                if abs(akse_verdi - siste_akse[i]) > terskel: #abs er absoluttverdi og koden sammenligner nåværende verdi og forrige og hvis forskjellen er større enn 0.05 printes det ut.
+                    #Gir navn til aksene på kontrollen
+                    if i == 0:
+                        navn = "Venstre joystick, Horisontal"
+                    elif i == 1:
+                        navn = "Venstre joystick, Vertikal"
+                    elif i == 2:
+                        navn = "Høyre joystick, Horisontal"
+                    elif i == 3:
+                        navn = "Høyre joystick, Vertikal"
+                    elif i == 4:
+                        navn = "LT (Venstre trigger)"
+                    elif i == 5:
+                        navn = "RT (Høyre trigger)"
+                    else:
+                        navn = f"Akse {i}"
+
+                    print(f"{navn} (akse {i}: {akse_verdi:.2f}" flush=TRUE)  #skriver ut navn, indeks og verdi med 2desimaler
+                    siste_akse[i] = akse_verdi #lagrer nye verdien for aksen i variabelen
+            except Exception as e:
+                print(f"Feil ved lesing av aksen {i}: {e}", flush=TRUE)  #skjer det en feil vil dette fange det opp og printe ut
 
         # Lesing av høyre joystick verdier (horisontal, venstre og høyre styring på bilen)
         right_stick_x = joystick.get_axis(3)
@@ -75,9 +118,6 @@ try:
         left_stick_x = joystick.get_axis(0)
         left_stick_y = joystick.get_axis(1)
 
-        print(
-            f"Venstre stikke: x={left_stick_x:.2f}, y={left_stick_y:.2f} | "
-            f"Høyre stikke: x={right_stick_x:.2f}, y={right_stick_y:.2f}")
         
         time.sleep(0.1)
 
