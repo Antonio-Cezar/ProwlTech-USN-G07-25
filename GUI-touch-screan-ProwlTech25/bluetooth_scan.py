f4ad4cebac918afdc5dd381e22aeaa2a7d78437e
AlphaLizzard <idalarsen993@gmail.com>
2025-04-08 14:25:45 +0200

    test

--- GUI-touch-screan-ProwlTech25/bluetooth_scan.py
+++ GUI-touch-screan-ProwlTech25/bluetooth_scan.py
@@ -1,5 +1,6 @@
 import subprocess
 import re
+import time
 
 def scan_bluetooth_devices(name_filter="Controller"):
     """
@@ -15,36 +16,27 @@ def scan_bluetooth_devices(name_filter="Controller"):
         text=True
     )
 
-    # Kjører begge kommandoene: devices og paired-devices
-    process.stdin.write('devices\n')
+    process.stdin.write('scan on\n')
+    process.stdin.flush()
+
+    time.sleep(5)
+
+    process.stdin.write('scan off\n')
     process.stdin.write('quit\n')
     process.stdin.flush()
 
     output, _ = process.communicate()
 
     devices = {}
-    seen = set()  # For å unngå duplikater
-    addresses = []
 
     for line in output.split('\n'):
         match = re.search(r'Device ([0-9A-F:]+) (.+)', line)
         if match:
             address, name = match.groups()
 
-            if name_filter.lower() in name.lower() and address not in seen:
-                addresses.append((address, name))
-                seen.add(address)
+            if name_filter.lower() in name.lower():
+                devices[address] = name
 
-    # Filtrer kun tilgjengelige enheter
-    for addr, name in addresses:
-        info = subprocess.run(
-            ['bluetoothctl', 'info', addr],
-            capture_output=True,
-            text=True
-        ).stdout
-
-        if "Connected: yes" in info or "Connected: no" in info:
-            devices[addr] = name
 
     return devices
 
