8966c737d88ed642570281945a01e0eb287fd781
AlphaLizzard <idalarsen993@gmail.com>
2025-04-08 13:52:05 +0200

    test

--- GUI-touch-screan-ProwlTech25/bluetooth_scan.py
+++ GUI-touch-screan-ProwlTech25/bluetooth_scan.py
@@ -16,8 +16,7 @@ def scan_bluetooth_devices(name_filter="Controller"):
     )
 
     # Kjører begge kommandoene: devices og paired-devices
-    process.stdin.write('devices\n')           # Alle kjente enheter
-    process.stdin.write('paired-devices\n')    # Tidligere parrede
+    process.stdin.write('devices\n')
     process.stdin.write('quit\n')
     process.stdin.flush()
 
@@ -25,6 +24,7 @@ def scan_bluetooth_devices(name_filter="Controller"):
 
     devices = {}
     seen = set()  # For å unngå duplikater
+    addresses = []
 
     for line in output.split('\n'):
         match = re.search(r'Device ([0-9A-F:]+) (.+)', line)
@@ -32,8 +32,19 @@ def scan_bluetooth_devices(name_filter="Controller"):
             address, name = match.groups()
 
             if name_filter.lower() in name.lower() and address not in seen:
-                devices[address] = name
+                addresses.append((address, name))
                 seen.add(address)
+
+    # Filtrer kun tilgjengelige enheter
+    for addr, name in addresses:
+        info = subprocess.run(
+            ['bluetoothctl', 'info', addr],
+            capture_output=True,
+            text=True
+        ).stdout
+
+        if "Connected: yes" in info or "Connected: no" in info:
+            devices[addr] = name
 
     return devices
 
