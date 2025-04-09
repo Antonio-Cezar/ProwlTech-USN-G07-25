74d25ed1b056a730cbdc3e09352993701ea44669
AlphaLizzard <idalarsen993@gmail.com>
2025-04-09 13:08:00 +0200

    test

--- GUI-touch-screan-ProwlTech25/bluetooth_scan2.py
+++ GUI-touch-screan-ProwlTech25/bluetooth_scan2.py
@@ -5,15 +5,19 @@ async def scan_devices():
     print("Søker etter enheter...\n")
     devices = await BleakScanner.discover(timeout=10.0)
 
-    found_name = False
+    name_filter = ["Controller"]
+
+    found_any = False
     for d in devices:
         if d.name:
-            print(f"{d.name} ({d.address})")
-            found_name = True
+            if any(filter_word in d.name.lower() for filter_word in name_filter):
+                print(f"{d.name} ({d.address})")
+                found_any = True
 
-    if not found_name:
+    if not found_any:
         print("Fant ingen enheter")
 
-asyncio.run(scan_devices())
+if __name__ == "__main__":
+    asyncio.run(scan_devices())
 
     
