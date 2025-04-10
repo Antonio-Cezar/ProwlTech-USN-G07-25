f126b2835b6383c407fb4b591b01fc0a4ab5db1c
AlphaLizzard <idalarsen993@gmail.com>
2025-04-10 14:52:58 +0200

    ny

--- GUI-touch-screan-ProwlTech25/gui_2.py
+++ GUI-touch-screan-ProwlTech25/gui_2.py
@@ -129,7 +129,7 @@ class ProwlTechApp(ctk.CTk):
         )
         self.can_bus_button.place(relx=0.2, rely=0.9, anchor="center")
 
-    def run_can_script():
+    def run_can_script(self):
         subprocess.Popen(["bash", "start_canbus_meny_konsoll.sh"])
 
     # Popup-vindu som vises når "Info"-knappen trykkes
