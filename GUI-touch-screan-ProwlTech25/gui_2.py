2e8a4c19c03018e2b03bcf913c55ee37514e2332
AlphaLizzard <idalarsen993@gmail.com>
2025-04-11 11:13:11 +0200

    canbus meny

--- GUI-touch-screan-ProwlTech25/gui_2.py
+++ GUI-touch-screan-ProwlTech25/gui_2.py
@@ -117,7 +117,7 @@ class ProwlTechApp(ctk.CTk):
         self.can_bus_button.place(relx=0.2, rely=0.9, anchor="center")
 
     def run_can_script(self):
-        subprocess.Popen(["bash", "../Raspberry-PI-4-scripts/start_canbus_meny_konsoll.sh"])
+        subprocess.Popen(["bash", "../Raspberry-PI-4-scripts/canbus_meny.sh"])
 
     # Popup-vindu som vises når "Info"-knappen trykkes
     def open_info_window(self):
