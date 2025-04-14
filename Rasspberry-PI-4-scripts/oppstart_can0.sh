d19f648c14aede5e01ddd96050b3a887429cfc98
Antonio-Cezar <cezaravadanei@gmail.com>
2025-04-14 10:50:18 +0200

    fix

--- Rasspberry-PI-4-scripts/oppstart_can0.sh
+++ Rasspberry-PI-4-scripts/oppstart_can0.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
-
 CAN_INTERFACE="can0"
 BITRATE=125000
 
-start_canbus() {
-    echo "Starter $CAN_INTERFACE med bitrate $BITRATE ..."
-    sudo ip link set $CAN_INTERFACE down
-    sudo ip link set $CAN_INTERFACE type can bitrate $BITRATE
-    sudo ip link set $CAN_INTERFACE up
-    sleep 1
-}
-
-show_status() {
-    echo ""
-    echo "=== CAN-status ==="
-    ip -details link show $CAN_INTERFACE | grep -A 5 "$CAN_INTERFACE"
-    echo ""
-}
-
-# Første oppstart
-start_canbus
-show_status
+ip link set $CAN_INTERFACE down
+ip link set $CAN_INTERFACE type can bitrate $BITRATE
+ip link set $CAN_INTERFACE up
