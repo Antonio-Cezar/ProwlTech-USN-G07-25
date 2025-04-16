70cf7af11d886c0e107562bb67bda2bdf4f3fae6
AlphaLizzard <idalarsen993@gmail.com>
2025-04-16 10:52:37 +0200

    fix

--- GUI-touch-screan-ProwlTech25/assets.py
+++ GUI-touch-screan-ProwlTech25/assets.py
@@ -1,25 +1,33 @@
 from PIL import Image
 import customtkinter as ctk
+import os
 
-def load_image(path, size):
-    return ctk.CTkImage(light_image=Image.open(path), size=size)
+# Finn stien til mappen som inneholder denne filen
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Full sti til Image-mappen
+IMAGE_DIR = os.path.join(BASE_DIR, "Image")
+
+def load_image(filename, size):
+    image_path = os.path.join(IMAGE_DIR, filename)
+    return ctk.CTkImage(light_image=Image.open(image_path), size=size)
 
 # Logoer
-prowltech_logo = load_image("Image/prowltech_logo2.png", (550, 550))
-usn_logo = load_image("Image/USN_logo.png", (60, 70))
+prowltech_logo = load_image("prowltech_logo2.png", (550, 550))
+usn_logo = load_image("USN_logo.png", (60, 70))
 
 # Ikoner
-info_icon = load_image("Image/info.png", (20, 20))
-can_icon = load_image("Image/can_bus.png", (20, 20))
-bluetooth_icon = load_image("Image/bluetooth.png", (20, 20))
-bolt_icon = load_image("Image/bolt.png", (20, 20))
-cross_icon = load_image("Image/cross.png", (20, 20))
-menu_icon = load_image("Image/menu.png", (20, 20))
-cross_icon = load_image("Image/cross.png", (15, 15))
-sensor_icon = load_image("Image/sensor.png", (20, 20))
-signal_icon = load_image("Image/signal.png", (15, 15))
-temp_icon = load_image("Image/temperature.png", (20, 20))
-update_icon = load_image("Image/update.png", (15, 15))
-warning_icon = load_image("Image/warning.png", (20, 20))
-loading_icon = load_image("Image/loading.png", (20, 20))
-start_icon = load_image("Image/start.png", (20, 20))
+info_icon = load_image("info.png", (20, 20))
+can_icon = load_image("can_bus.png", (20, 20))
+bluetooth_icon = load_image("bluetooth.png", (20, 20))
+bolt_icon = load_image("bolt.png", (20, 20))
+cross_icon = load_image("cross.png", (15, 15))
+sensor_icon = load_image("sensor.png", (20, 20))
+signal_icon = load_image("signal.png", (15, 15))
+temp_icon = load_image("temperature.png", (20, 20))
+update_icon = load_image("update.png", (15, 15))
+warning_icon = load_image("warning.png", (20, 20))
+loading_icon = load_image("loading.png", (20, 20))
+start_icon = load_image("start.png", (20, 20))
+menu_icon = load_image("menu.png", (20, 20))
+
