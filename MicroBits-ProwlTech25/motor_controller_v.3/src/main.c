1b1f688efef4d78ee3cc3fb43882da839687cb4f
RameezSadiq <112103778+RameezSadiq@users.noreply.github.com>
2025-04-17 18:36:33 +0200

    updatet main for testing av uart
    kommenterte ut can-del og la til eksempeltest for å teste koden.

--- MicroBits-ProwlTech25/motor_controller_v.3/src/main.c
+++ MicroBits-ProwlTech25/motor_controller_v.3/src/main.c
@@ -17,24 +17,43 @@
 
 
  int main(void) {
-    printf("Starter CAN...\n");
-    const struct device *const can_dev = get_can_dev(); // Getting the CAN device connected to the system
-    canBegin(can_dev); // Function for startring up and initializing the CAN device
-    printf("CAN er klar. \n");
-
-    k_msleep(10);
+    printf("Starter UART...\n");
+    //const struct device *const can_dev = get_can_dev(); // Getting the CAN device connected to the system
+    //canBegin(can_dev); // Function for startring up and initializing the CAN device
+    //printf("CAN er klar. \n");
 
     uart_init(); //initialiserer uart
 
+    k_msleep(10);
+
+    float test_fart = 0.5f;
+    float test_vinkel = 0.0f;
+    float test_rotasjon = 0.0f;
+
     while (1) {
         printf("In the while loop\n");
-        canState(can_dev);// Function for checking the CAN state
-        process_can_data(); // Using the process_can_data function
+        //canState(can_dev);// Function for checking the CAN state
+        //process_can_data(); // Using the process_can_data function
         //if (er_nødstopp_aktivert()) { 
             //printf("nødstopp aktivert\n"); // RAS og linja over
         //}
 
-        send_string(can_dev, "Hello"); // Using the send_string function to send the string "Hello"
+        //FOR Testing: kjører eksempelverdier
+        
+        printf("tester motor: fart=%.2f, vinkel=%.2f, rotasjon=%.2f\n");
+
+        kontroller_motorene(test_fart, test_vinkel, test_rotasjon);
+
+        
+        test_fart += 0.1f; // Øk farten litt hver runde
+
+        if (test_fart > 1.0f) {
+            test_fart = 0.0f; // Når vi når max fart, start på nytt
+        }
+
+        k_msleep(500); // Vent 500ms mellom hver endring
+
+        //send_string(can_dev, "Hello"); // Using the send_string function to send the string "Hello"
         k_msleep(50); // Sleep for 50ms, letting the callback handle messages
     }
 }
