86a47b809209bb89efb2fa3a235e68a6ae9c5be4
RameezSadiq <112103778+RameezSadiq@users.noreply.github.com>
2025-04-22 14:58:14 +0200

    Update test_vesc.py

--- Rasspberry-PI-4-scripts/test_vesc.py
+++ Rasspberry-PI-4-scripts/test_vesc.py
@@ -1,20 +1,20 @@
 import serial
 import time
 
-# Bytt ut 'COM9' med porten til din USB‑TTL
-ser = serial.Serial('COM9', 115200, timeout=1)
+# Bruk riktig enhet her:
+ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
 
-# Vent litt om nødvendig
+# Gi litt tid til at UARTen kommer opp
 time.sleep(0.1)
 
-# Bygg frame
+# Bygg VESC‑rute
 frame = bytes([0x02, 0x05, 0x05, 0x00, 0x00, 0x00, 0x3F, 0xE4, 0xEB, 0x03])
 
 print("Sender:", frame.hex(' '))
 ser.write(frame)
 
-# (Valgfritt) les tilbake om VESC svarer
+# (Valgfritt) les eventuelt svar fra VESC
 resp = ser.read(32)
-print("Mottatt :", resp.hex(' '))
+print("Mottatt:", resp.hex(' '))
 
 ser.close()
