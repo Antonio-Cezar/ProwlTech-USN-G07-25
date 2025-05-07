46fe1c1bc604986a512c120cefa1d2ac06394d12
AlphaLizzard <idalarsen993@gmail.com>
2025-05-07 14:44:51 +0200

    sletter ubrukte koder

--- GUI-touch-screan-ProwlTech25/battery_reader.py
+++ GUI-touch-screan-ProwlTech25/battery_reader.py
@@ -3,19 +3,20 @@ import time
 
 def read_battery_percentage():
     try:
+        # Åpner seriallport ttyS0 med 9600 bps
         with serial.Serial('/dev/ttyS0', baudrate=9600, timeout=1) as ser:
-            print("Lytter på batteridata fra TF03K...\n")
+            print("Lytter på batteridata fra shunt...\n")
 
             while True:
                 frame = ser.read(16)
 
-                if len(frame) != 16:
+                if len(frame) != 16:    # Leser nøyaktig 16 byte, hvis færre, hopp over
                     continue
 
                 if frame[0] != 0xA5 or (sum(frame[0:15]) & 0xFF) != frame[15]:  # sum av byte skal stemme med checksum
                         continue 
 
-                percent = frame[1]
+                percent = frame[1]  # Parsing
                 print(f"Batteriprosent: {percent}%")
                 time.sleep(1)
 
