91fdd8406e8b1987b7e0c6dde6c4541b63b1df3b
AlphaLizzard <idalarsen993@gmail.com>
2025-05-18 12:36:47 +0200

    rydde

--- GUI-touch-screan-ProwlTech25/shunt_data.py
+++ GUI-touch-screan-ProwlTech25/shunt_data.py
@@ -42,13 +42,13 @@ else:
 
                 # Parsing
                 soc = frame[1]
-                voltage = ((frame[2] << 8) | frame[3]) / 100.0  # Spenning: byte 2 og 3 (16-bit tall), del på 100 for volt
-                capacity = struct.unpack('>I', frame[4:8])[0]   # Kapasitet (mAh) (32-bit tall)
-                current = struct.unpack('>i', frame[8:12])[0]   # Strøm (mA) (32-bit tall)
-                t = (frame[12] << 16) | (frame[13] << 8) | frame[14]    # Resterende tid i sekunder (leser 3 byte)
-                hrs, rem = divmod(t, 3600)  # Timer og restsekunder
-                mins, secs = divmod(rem, 60)    # Minutter og restsekunder
-                remaining = f"{hrs:02d}:{mins:02d}:{secs:02d}"
+                #voltage = ((frame[2] << 8) | frame[3]) / 100.0  # Spenning: byte 2 og 3 (16-bit tall), del på 100 for volt
+                #capacity = struct.unpack('>I', frame[4:8])[0]   # Kapasitet (mAh) (32-bit tall)
+                #current = struct.unpack('>i', frame[8:12])[0]   # Strøm (mA) (32-bit tall)
+                #t = (frame[12] << 16) | (frame[13] << 8) | frame[14]    # Resterende tid i sekunder (leser 3 byte)
+                #hrs, rem = divmod(t, 3600)  # Timer og restsekunder
+                #mins, secs = divmod(rem, 60)    # Minutter og restsekunder
+                #remaining = f"{hrs:02d}:{mins:02d}:{secs:02d}"
                 return soc
 
         except Exception as e:
